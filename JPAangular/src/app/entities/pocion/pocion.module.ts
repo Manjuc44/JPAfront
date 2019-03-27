6e2393df588642c9a6a066ccbf93f26caa5ee9fa
Edu Navajas <eduardo.navajascortes@gmail.com>
2019-03-27 16:49:52 +0100

    Añadido home funcional y creada la entittie de Detail y Grid (vacias)

--- JPAangular/src/app/entities/pocion/pocion.module.ts
+++ JPAangular/src/app/entities/pocion/pocion.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PocionRoutingModule } from './pocion-routing.module';
+import { PocionDetailComponent } from './pocion-detail/pocion-detail.component';
+import { PocionGridComponent } from './pocion-grid/pocion-grid.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PocionDetailComponent, PocionGridComponent],
   imports: [
     CommonModule,
     PocionRoutingModule
