a2dae3ae102753a4eba209ec78421bb38b72c942
manuel.cerda <Manjuc4499@gmail.com>
2019-03-27 16:40:26 +0100

    estructure part 1 entities

---
+++ JPAangular/src/app/entities/pocion/pocion.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PocionRoutingModule } from './pocion-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PocionRoutingModule
+  ]
+})
+export class PocionModule { }
